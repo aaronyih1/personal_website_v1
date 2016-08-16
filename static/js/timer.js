eb1b14557edfb30157fc534a00731f04d3cbb703
Aaron Yih <aaron.yih@ucla.edu>
2016-08-15 22:40:40 -0700

    first version of HUD

--- static/js/timer.js
+++ static/js/timer.js
@@ -1,9 +1,9 @@
-var msec = Date.parse("March 21, 2012");
+var msec = Date.parse("Auguest 12, 2016 19:32:24");
 console.log("hello");
 window.setInterval(upTime, 1000);
 
 function upTime() {
 	msec+=1000;
-	console.log(msec);
 	var d = new Date(msec);
 	document.getElementById("insert_date").innerHTML = d;
+}
